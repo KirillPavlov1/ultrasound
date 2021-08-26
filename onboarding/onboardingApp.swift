7735bfc21a77e21cb4b19ec138b8ddef506e7346
Kirill <pavlov140700@mail.ru>
2021-08-26 19:15:34 +0300

    Initial Commit

---
+++ onboarding/onboardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  onboardingApp.swift
+//  onboarding
+//
+//  Created by Кирилл on 26.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct onboardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
